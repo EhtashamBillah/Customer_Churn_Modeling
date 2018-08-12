1fd029aca992b240eff3250e4ec2f9fe1f971024
Mohammad Ehtasham Billah <mymun.stat@gmail.com>
2018-08-12 09:45:28 +0200

    Update customer_churn_modeling.R

--- customer_churn_modeling.R
+++ customer_churn_modeling.R
@@ -1,15 +1,16 @@
 
-
-
 # Importing the dataset
 dataset <- read.csv('Churn_Modelling.csv')
+
 # keeping the necessary columns
 dataset <- dataset[,4:14]
 str(dataset)
+
 # Creating the dummy variable for Geography and Gender variable
 dataset$Geography <-factor(dataset$Geography,levels=c("France","Spain","Germany"),labels =c(1,2,3))
 dataset$Gender <-factor(dataset$Gender,levels=c("Female","Male"),labels =c(1,2))
-# XGBOOST want to treat the dummy variable as numeric value.so we need to transform it to numeric
+
+# XGBOOST treats the dummy variable as numeric value.so we need to transform it into numeric
 dataset$Geography <-as.numeric(dataset$Geography)
 dataset$Gender <-as.numeric(dataset$Gender)
 
@@ -29,7 +30,6 @@ model_xgboost <- xgboost(data = as.matrix(training_set[,-11]), label = training_
 # we want to EVALUATE xgboost model using k-fold cross validation
 require(caret)
 folds <- createFolds(training_set$Exited, k=10) # 10 different test folds composing training set
-folds
 cv <- lapply(folds,function(x){ # x is each observation of all 10 test folds
   training_fold <- training_set[-x,] # whole training set except the 10 test fold
   test_fold <- training_set[x,]
@@ -46,11 +46,10 @@ mean(as.numeric(cv))
 # pridicting y of test set
 y_hat <- predict(model_xgboost, newdata = as.matrix(test_set[,-11]))
 y_hat <- ifelse(y_hat>0.5,1,0)
-y_hat
 
 # confussin matrix
 cm <- table(test_set[,11],y_hat)
-cm
+
 #accuracy
 accuracy <- (cm[1,1]+cm[2,2])/(cm[1,1]+cm[2,2]+cm[1,2]+cm[2,1])
-accuracy
+
